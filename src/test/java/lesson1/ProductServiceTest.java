d95cc87dcc7d6e5c556ab8af5cfa42da5d8f7d2e
Andrey <k-udm@ya.ru>
2023-11-17 10:27:12 +0100

    Mockito. Mock and spy.

--- src/test/java/lesson1/ProductServiceTest.java
+++ src/test/java/lesson1/ProductServiceTest.java
@@ -66,4 +66,65 @@ class ProductServiceTest {
         assertNotNull(product);
         assertEquals(testArticle, product.getArticle());
     }
+
+    @Test
+    public void checkFullDeletion() {
+        when(merchService.fullDeletion()).thenReturn(true);
+
+        Product testProduct = new Product("Test name", 777, "Test supplier");
+
+        testProduct = service.save(testProduct);
+        int id = testProduct.getId();
+
+        service.delete(id);
+
+        testProduct = service.getById(id);
+
+        assertNull(testProduct);
+    }
+
+    @Test
+    public void checkIfInactiveStatusWasSet() {
+
+        Product testProduct = new Product("Test name", 777, "Test supplier");
+
+        testProduct = service.save(testProduct);
+        int id = testProduct.getId();
+
+        service.delete(id);
+
+        testProduct = service.getById(id);
+
+        assertNotNull(testProduct);
+        assertFalse(testProduct.isActive());
+    }
+
+    @Test
+    public void mockAndSpyDemo() {
+        Product realProduct = new Product("Real name", 555, "Real supplier");
+        service.save(realProduct);
+
+        ProductService mock = mock(ProductService.class);
+        ProductService spy = spy(service);
+
+        mock.save(realProduct);
+
+        Product product1 = new Product("Test name 1", 111, "Test supplier 1");
+        Product product2 = new Product("Test name 2", 222, "Test supplier 2");
+
+        List<Product> products = List.of(product1, product2);
+
+        when(mock.getAll()).thenReturn(products);
+        when(spy.getAll()).thenReturn(products);
+
+        System.out.println();
+        System.out.println("Переопределённое поведение mock и spy:");
+        System.out.println("Mock: " + mock.getAll());
+        System.out.println("Spy:  " + spy.getAll());
+
+        System.out.println();
+        System.out.println("Если мы не переопределяем поведение mock и spy:");
+        System.out.println("Mock: " + mock.getById(4));
+        System.out.println("Spy:  " + spy.getById(4));
+    }
 }
